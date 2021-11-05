f562abcdf0c463358a48f0a7be5d63211efd17c3
ram5353 <ramkiran5353@gmail.com>
2021-11-05 18:02:08 -0400

    set cross origin and update application.properties

--- src/main/java/com/dev/retailwebbackend/controller/HouseHoldController.java
+++ src/main/java/com/dev/retailwebbackend/controller/HouseHoldController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:8080")
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/house-holds")
 public class HouseHoldController {
